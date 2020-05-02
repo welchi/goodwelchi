4702e356f224b2f395734c1062b956888e51c384
welchi <welchizm@gmail.com>
2020-05-02 20:53:07 +0900

    Add top_image

---
+++ lib/widget/top_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TopImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/top.png',
+    );
+  }
+}
