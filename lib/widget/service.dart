9668f170b990d30a7fdf52463bfd426b90955119
welchi <welchizm@gmail.com>
2020-05-02 20:57:58 +0900

    Add Service

---
+++ lib/widget/service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Service extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const <Widget>[
+        Text(
+          'Service',
+        ),
+      ],
+    );
+  }
+}
