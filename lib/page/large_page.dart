07ed584878605706df3b84ad4a43c1611e133a13
welchi <welchizm@gmail.com>
2020-05-02 20:59:03 +0900

    Update top page to show Service

--- lib/page/large_page.dart
+++ lib/page/large_page.dart
@@ -11,6 +11,7 @@ class LargePage extends StatelessWidget {
             Logo(),
             Nav(),
             TopImage(),
+            Service(),
           ],
         ),
       ),
--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,3 +1,4 @@
 export 'logo.dart';
 export 'nav.dart';
+export 'service.dart';
 export 'top_image.dart';
