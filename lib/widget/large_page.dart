6d7ebd1bcc1352c7b725748e8f79668a3bee8943
welchi <welchizm@gmail.com>
2020-05-02 19:21:23 +0900

    Update LargePage

--- lib/widget/large_page.dart
+++ lib/widget/large_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class LargePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
