7b038bd7ed0cff78818d443e64e574faad8f38c1
welchi <welchizm@gmail.com>
2020-05-02 21:35:28 +0900

    Update to add nav buttons data

--- lib/widget/nav.dart
+++ lib/widget/nav.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Nav extends StatelessWidget {
+  List<NavButtonData> navButtons = [
+    NavButtonData(
+      title: 'Home',
+      onPressed: () {},
+    ),
+    NavButtonData(
+      title: 'About',
+      onPressed: () {},
+    ),
+    NavButtonData(
+      title: 'Works',
+      onPressed: () {},
+    ),
+    NavButtonData(
+      title: 'Contact',
+      onPressed: () {},
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Text('ナビゲーション');
