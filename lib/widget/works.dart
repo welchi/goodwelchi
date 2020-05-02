0a7e1cdcf40d26375d4059398b97977750941dc8
welchi <welchizm@gmail.com>
2020-05-02 22:46:54 +0900

    Update Service font size

--- lib/widget/works.dart
+++ lib/widget/works.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class Works extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text('Works');
+    return Text(
+      'Works',
+      style: Theme.of(context).textTheme.headline4,
+    );
   }
 }
