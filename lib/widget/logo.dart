bb543352ebc77897fdd6401c07395a1c00746642
welchi <welchizm@gmail.com>
2020-05-02 21:23:39 +0900

    Update to show logo image

--- lib/widget/logo.dart
+++ lib/widget/logo.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text('GoodWeldhi');
+    return Row(
+      children: <Widget>[
+        SizedBox(
+          height: 60,
+          width: 60,
+          child: Image.asset(
+            'assets/images/logo.png',
+          ),
+        ),
+        const Text(
+          'GoodWelchi',
+        ),
+      ],
+    );
   }
 }
