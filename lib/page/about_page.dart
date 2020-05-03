6cea9da3dd91c364196c9483c726ba5075da0421
welchi <welchizm@gmail.com>
2020-05-03 19:40:31 +0900

    Add AboutPage

---
+++ lib/page/about_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  static String routeName = 'about';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text(
+          'Hello',
+        ),
+      ),
+    );
+  }
+}
