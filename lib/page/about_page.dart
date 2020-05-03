0731f6965bbba9f0279d9a12a9fc3d87d8df5e40
welchi <welchizm@gmail.com>
2020-05-03 23:07:55 +0900

    Update AboutPage to add AppBar

--- lib/page/about_page.dart
+++ lib/page/about_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:goodwelchi/util/util.dart';
 
 class AboutPage extends StatelessWidget {
   static String routeName = 'about';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: getNavigatorAppBar(
+        context,
+      ),
       body: Container(
         child: const Text(
           'Hello',
