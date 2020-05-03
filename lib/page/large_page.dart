f10adc49e26145b54432b30385b2aadd5324592d
welchi <welchizm@gmail.com>
2020-05-03 19:57:10 +0900

    Update to add AppBar

--- lib/page/large_page.dart
+++ lib/page/large_page.dart
@@ -5,6 +5,19 @@ class LargePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: SizedBox(
+          height: 60,
+          width: 60,
+          child: Image.asset(
+            'assets/images/logo.png',
+          ),
+        ),
+        title: Text(
+          'GoodWelchi',
+          style: Theme.of(context).textTheme.headline3,
+        ),
+      ),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverList(
