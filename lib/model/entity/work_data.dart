0a5278048fe8b73b3c87f1601e48ec16ca4fa225
welchi <welchizm@gmail.com>
2020-05-03 16:28:30 +0900

    Update WorkData

--- lib/model/entity/work_data.dart
+++ lib/model/entity/work_data.dart
@@ -1,1 +1,14 @@
-class WorkData {}
+import 'package:flutter/cupertino.dart';
+
+class WorkData {
+  const WorkData({
+    @required this.imgPath,
+    @required this.title,
+    @required this.description,
+    this.url,
+  });
+  final String imgPath;
+  final String title;
+  final String description;
+  final String url;
+}
