a225a23556033dadea87fa499b70705fd6aeee0f
welchi <welchizm@gmail.com>
2020-05-03 19:31:54 +0900

    Update padding of top image

--- lib/widget/top_image.dart
+++ lib/widget/top_image.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class TopImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/images/top.png',
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 16,
+        bottom: 16,
+      ),
+      child: Image.asset(
+        'assets/images/top.png',
+      ),
     );
   }
 }
