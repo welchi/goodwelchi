d8517fa7b056a3cfcab0f84addc6779c55bc89fb
welchi <welchizm@gmail.com>
2020-05-04 22:54:01 +0900

    Add external link data

--- lib/model/entity/entity.dart
+++ lib/model/entity/entity.dart
@@ -1,4 +1,5 @@
 export 'device_size.dart';
+export 'external_link_data.dart';
 export 'nav_button_data.dart';
 export 'service_content_data.dart';
 export 'work_data.dart';
---
+++ lib/model/entity/external_link_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExternalLinkData {
+  ExternalLinkData({
+    @required this.assetPath,
+    @required this.url,
+  });
+  final String assetPath;
+  final String url;
+}
