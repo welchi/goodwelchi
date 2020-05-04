e82e8ea12706f9b4215782b12ecbe953a4de571e
welchi <welchizm@gmail.com>
2020-05-04 18:32:13 +0900

    Update service font

--- lib/widget/service.dart
+++ lib/widget/service.dart
@@ -77,7 +77,7 @@ class _ServiceContent extends StatelessWidget {
         children: <Widget>[
           Text(
             title,
-            style: Theme.of(context).textTheme.headline6,
+            style: Theme.of(context).textTheme.headline5,
           ),
           Padding(
             padding: const EdgeInsets.all(8),
