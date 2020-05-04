18cf1912bca7ee9edf0efdc9260835ca0162b626
welchi <welchizm@gmail.com>
2020-05-04 14:24:52 +0900

    Add WorksPage

---
+++ lib/page/works_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:goodwelchi/util/util.dart';
+
+class WorksPage extends StatelessWidget {
+  static String routeName = 'works';
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: getNavigatorAppBar(context),
+      body: Container(),
+    );
+  }
+}
