2be53f5ac72fb7a7c8b8b81107d546195fb2bd31
welchi <welchizm@gmail.com>
2020-05-04 14:38:00 +0900

    Update WorksPage

--- lib/page/works_page.dart
+++ lib/page/works_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:goodwelchi/util/util.dart';
+import 'package:goodwelchi/widget/widget.dart';
 
 class WorksPage extends StatelessWidget {
   static String routeName = 'works';
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getNavigatorAppBar(context),
-      body: Container(),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: works
+            .map(
+              (work) => Padding(
+                padding: EdgeInsets.all(
+                  16,
+                ),
+                child: InkWell(
+                  child: Card(
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
