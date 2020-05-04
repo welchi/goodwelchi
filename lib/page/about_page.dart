077b8cf58b8187eeab659f3a767b0af17f166b69
welchi <welchizm@gmail.com>
2020-05-04 22:33:43 +0900

    Update bubble

--- lib/page/about_page.dart
+++ lib/page/about_page.dart
@@ -13,7 +13,7 @@ class AboutPage extends StatelessWidget {
       endDrawer: getNavigatorDrawer(context),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Row(
               children: <Widget>[
@@ -30,22 +30,35 @@ class AboutPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Bubble(
-                        nip: BubbleNip.leftBottom,
-                        color: const Color(
-                          0xFFB0F2DA,
-                        ),
-                        child: Text(
-                          'こんにちは！バーチャル美少女エンジニアの「うぇるち」です！',
-                          style: Theme.of(context).textTheme.headline5,
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Bubble(
+                          padding: const BubbleEdges.all(
+                            8,
+                          ),
+                          radius: const Radius.circular(
+                            20,
+                          ),
+                          nip: BubbleNip.leftBottom,
+                          color: const Color(
+                            0xFFB0F2DA,
+                          ),
+                          child: Text(
+                            'こんにちは！バーチャル美少女エンジニアの「うぇるち」です！',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
                         ),
                       ),
-                      const Text(
-                        'アバターのままフリーランスに挑戦中です。 '
-                        '企画・開発・運用まで、手広く対応しています。'
-                        'iOS・Androidクロスプラットフォーム開発から、'
-                        'Unityを使ったゲーム関連開発まで、幅広い分野を扱っています。'
-                        '漫画家「吉野ホダカ」と提携しており、イラスト・漫画・デザイン全般も対応可能です。',
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          'アバターのままフリーランスに挑戦中です。 '
+                          '企画・開発・運用まで、手広く対応しています。'
+                          'iOS・Androidクロスプラットフォーム開発から、'
+                          'Unityを使ったゲーム関連開発まで、幅広い分野を扱っています。'
+                          '漫画家「吉野ホダカ」と提携しており、イラスト・漫画・デザイン全般も対応可能です。',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
                       ),
                     ],
                   ),
