5dc030193f68af655f2934eb49c9a7b8f2e1ea10
welchi <welchizm@gmail.com>
2020-05-05 20:44:14 +0900

    Update to uncheck debug

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class GoodWelchi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GoodWelchi',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
