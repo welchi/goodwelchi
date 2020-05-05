e1cb5103f968291bcee93fb99e2ebed457e4b76c
welchi <welchizm@gmail.com>
2020-05-05 22:04:16 +0900

    Add get_copyright

---
+++ lib/util/get_copyright.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Widget getCopyright() {
+  return const Padding(
+    padding: EdgeInsets.all(
+      16,
+    ),
+    child: Text(
+      '©︎2020 GoodWelchi',
+    ),
+  );
+}
