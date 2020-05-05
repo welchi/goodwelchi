3dcfe6442d5ec3b06e28cbd2a0197c131bfe11ca
welchi <welchizm@gmail.com>
2020-05-05 22:05:59 +0900

    Update to add copyright

--- lib/page/large_page.dart
+++ lib/page/large_page.dart
@@ -28,6 +28,13 @@ class LargePage extends StatelessWidget {
           const WorksGridView(
             deviceSize: DeviceSize.pc,
           ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                getCopyright(),
+              ],
+            ),
+          ),
         ],
       ),
     );
