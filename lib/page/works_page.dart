33e65d8448f79e20546c49ace185f420bd741157
welchi <welchizm@gmail.com>
2020-05-05 12:34:27 +0900

    Update works page

--- lib/page/works_page.dart
+++ lib/page/works_page.dart
@@ -9,33 +9,46 @@ class WorksPage extends StatelessWidget {
     return Scaffold(
       appBar: getNavigatorAppBar(context),
       endDrawer: getNavigatorDrawer(context),
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: works
-            .map(
-              (work) => Padding(
-                padding: const EdgeInsets.all(
-                  16,
-                ),
-                child: InkWell(
-                  onTap: () {
-                    showGridDetail(work, context);
-                  },
-                  child: Card(
-                    child: SizedBox(
-                      width: 200,
-                      height: 200,
-                      child: Image.asset(
-                        work.imgPath,
-                        fit: BoxFit.cover,
-                      ),
+      body: ResponsiveLayout(
+        smallWidget: getWorksGrid(
+          context,
+          2,
+        ),
+        largeWidget: getWorksGrid(
+          context,
+          3,
+        ),
+      ),
+    );
+  }
+
+  Widget getWorksGrid(BuildContext context, int count) {
+    return GridView.count(
+      crossAxisCount: count,
+      children: works
+          .map(
+            (work) => Padding(
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              child: InkWell(
+                onTap: () {
+                  showGridDetail(work, context);
+                },
+                child: Card(
+                  child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset(
+                      work.imgPath,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
-            )
-            .toList(),
-      ),
+            ),
+          )
+          .toList(),
     );
   }
 }
